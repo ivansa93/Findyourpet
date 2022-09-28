dba7fb20ea2e19df66823852125b280484c3b8ec
Iván Sará <ivansa93@hotmail.com>
2022-09-27 22:00:38 -0500

    Commit 1 Backend con conexión a base de datos

---
+++ Findyourpet/src/main/java/com/utp/Findyourpet/FindyourpetApplication.java
@@ -0,0 +1,13 @@
+package com.utp.Findyourpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyourpetApplication.class, args);
+	}
+
+}
